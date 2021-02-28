78ff18ce01763a90f1e52e931cfe86e4cae37d16
Ray <metaxray@outlook.com>
2021-03-01 00:01:56 +0800

    commit

--- chat_robot/run.py
+++
@@ -1,7 +0,0 @@
-#! -*- coding=utf-8 -*-
-import os, sys
-# sys.path.append(os.path.join(os.path.dirname(__file__), '.', 'core'))
-from core.websocket.ws_server import WsServer
-
-if __name__ == "__main__":
-    WsServer().server_run()
