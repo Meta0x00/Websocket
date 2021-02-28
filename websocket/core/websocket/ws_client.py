404b3e3efc81b843318aae4a5cc44cffc1641727
Ray <metaxray@outlook.com>
2021-03-01 00:19:43 +0800

    commit

--- websocket/core/websocket/ws_client.py
+++ websocket/core/websocket/ws_client.py
@@ -1,11 +1,14 @@
 #! -*- coding=utf-8 -*-
 import asyncio
 import websockets
+import json
+from websocket import create_connection
 
 class WsClient(object):
     def __init__(self):
         self.port = 7001
         self.host = "localhost"
+
     async def client_run(self):
         async with websockets.connect(f"ws://{self.host}:{self.port}") as ws:
             msg = input("Input: ")
@@ -13,5 +16,14 @@ class WsClient(object):
             await ws.send(msg)
             recv = await ws.recv()
             print(f"< From server {recv}")
+
+    async def client_run_ex(self):
+        ws = create_connection(f"ws://{self.host}:{self.port}")
+        msg = {"Hello World"}
+        await ws.send(json.dumps(msg))
+        print(f"> Send {msg}")
+        recv = await ws.recv()
+        print(f"< From server {recv}")
+        
 
 asyncio.get_event_loop().run_until_complete(WsClient().client_run())
